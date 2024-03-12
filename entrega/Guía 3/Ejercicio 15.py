56cf6d41e775213272885b1abcf0a312d7eeef1f
Facundo Palacios <161514095+reddeinternetde304mb@users.noreply.github.com>
2024-03-12 23:30:41 +0000

    Agrego el ejercicio 11 y 15 de la guía 3

--- entrega/Guía 3/Ejercicio 15.py
+++ entrega/Guía 3/Ejercicio 15.py
@@ -4,13 +4,18 @@ numero_limite = int(numero / divisor)
 is_primo = False
 is_compuesto = False
 
-while is_primo == True or is_compuesto == True:
-    if divisor > numero_limite:
-        print(f"{numero} es primo")
-        is_primo = True
-    if numero % divisor != 0:
-        divisor += 1
-        numero_limite = int(numero / divisor)
-    else:
-        print(f"{numero} es compuesto")
-        is_compuesto = True
+if numero == 1:
+    print(f"El número {numero} no es ni primo ni compuesto")
+elif numero < 1:
+    print(f"El número {numero} no es natural")
+else:
+    while is_primo == False and is_compuesto == False:
+        if divisor > numero_limite:
+            print(f"El número {numero} es primo")
+            is_primo = True
+        elif numero % divisor != 0:
+            divisor += 1
+            numero_limite = int(numero / divisor)
+        else:
+            print(f"El número {numero} es compuesto")
+            is_compuesto = True
