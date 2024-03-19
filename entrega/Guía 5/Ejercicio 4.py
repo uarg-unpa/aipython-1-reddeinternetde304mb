94c14057ccfc37e6f2093d81ab781a6efe6ed513
Facundo Palacios <161514095+reddeinternetde304mb@users.noreply.github.com>
2024-03-19 19:25:14 +0000

    Hice un cambio al ejercicio 4 de la guía 5, ahora usa del list

--- entrega/Guía 5/Ejercicio 4.py
+++ entrega/Guía 5/Ejercicio 4.py
@@ -2,8 +2,7 @@ frutas_favoritas_yum = ["Manzana", "Banana", "Mandarina", "Uva", "Frutilla", "Na
 
 print(frutas_favoritas_yum)
 print(f"a) {len(frutas_favoritas_yum)}")
-manzana_no_puaj = frutas_favoritas_yum[1:]
-print(f"b) {manzana_no_puaj}")
-manzana_no_pero_pera_si_yay = manzana_no_puaj
-manzana_no_pero_pera_si_yay.append("Pera")
-print(f"c) {manzana_no_pero_pera_si_yay}")
+del frutas_favoritas_yum[0]
+print(f"b) {frutas_favoritas_yum}")
+frutas_favoritas_yum.append("Pera")
+print(f"c) {frutas_favoritas_yum}")
